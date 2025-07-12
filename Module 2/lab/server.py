926664a49afe83de5d95a763e7f4f41d4125d97a
Dhikshil <mdhikshil@gmail.com>
2025-07-12 14:27:51 +0100

    added to server.py

--- Module 2/lab/server.py
+++ Module 2/lab/server.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 
-def home():
-    return "hello world"
+#def home():
+    #return "hello world"
 
 def index():
     return {"message": "hello world"}
